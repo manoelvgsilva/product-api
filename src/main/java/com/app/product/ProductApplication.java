7fad78b65847d4a22056f8f2e327b92e6b1b971d
manoelvgsilva <manoelvgsilva@gmail.com>
2024-08-27 13:12:38 -0300

    initial config

--- src/main/java/com/app/product/ProductApplication.java
+++ src/main/java/com/app/product/ProductApplication.java
@@ -3,11 +3,19 @@ package com.app.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * productapplication.
+ */
 @SpringBootApplication
 public class ProductApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
-	}
+  /**
+   * methodmain.
+   *
+   * @param args the args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ProductApplication.class, args);
+  }
 
 }
