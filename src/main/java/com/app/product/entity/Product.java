8afee2860825835d55bbcee2343846920739d20f
manoelvgsilva <manoelvgsilva@gmail.com>
2024-09-24 22:00:29 -0300

    correction errors

--- src/main/java/com/app/product/entity/Product.java
+++ src/main/java/com/app/product/entity/Product.java
@@ -1,32 +1,19 @@
 package com.app.product.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.util.Objects;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * product.
  */
-@Entity
-@Table(name = "products")
-@EntityListeners(AuditingEntityListener.class)
+@Document(collection = "products")
 public class Product {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String description;
   private String category;
   private Double price;
   private String font;
-  @Column(unique = true)
   private Long code;
   private Double weight;
 
